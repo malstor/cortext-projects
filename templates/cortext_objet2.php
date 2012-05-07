e652019b4c1bd30c6ed3ae2c47219cd202d46f68
chloeduloquin <chloeduloquin@hotmail.fr>
2012-05-07 15:51:36 +0200

    template 2

---
+++ templates/cortext_objet2.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Cortext Manager</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Plateforme Cortext Manager" />
+<meta name="keywords" content="manager,corpus,texte" />  
+<link href="css/reset.css" type="text/css" rel="stylesheet" />  
+<link href="css/style-general.css" type="text/css" rel="stylesheet" />
+<link href="css/style-general.css" type="text/css" rel="stylesheet" />
+<link href="css/style-menu.css" type="text/css" rel="stylesheet" />
+<link href="css/sidebar-left2.css" type="text/css" rel="stylesheet" />
+<link href="css/sidebar-right2-test.css" type="text/css" rel="stylesheet" />
+<link href="css/form-login.css" type="text/css" rel="stylesheet" />
+<link href="css/style-objet.css" type="text/css" rel="stylesheet" />
+	<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/canvas_barreprogress.js"></script>
+<script type="text/javascript" src="js/fonctions_form_barre_comment.js"></script>
+<script type="text/javascript" src="js/jquery.autogrowtextarea.js"></script>
+<script src="js/lightbox.js"></script>
+<script>
+  jQuery(document).ready(function($) {
+      $('a').smoothScroll({
+        speed: 1000,
+        easing: 'easeInOutCubic'
+      });
+
+      $('.showOlderChanges').on('click', function(e){
+        $('.changelog .old').slideDown('slow');
+        $(this).fadeOut();
+        e.preventDefault();
+      })
+  });
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2196019-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+<body onload="">
+  	  	
+<div id="conteneur-principal">
+
+	<div id="barre-top">
+    <div id="menu">
+    <a href="#"><img src="images/home2.png" alt="#" /></a>     
+    <ul>
+    <li class="menu1"><a href="#"> algues vertes</a></li> 
+    <li class="menu1b"><a href="#"> analysis.py - 12.12.2012</a></li>
+    <li class="menu1c"><a href="#"> carte semantique</a></li>   
+   	<li class="menu2"><a id="login-link" href="#" title="Login"> tam kien duong</a></li>
+    </ul>    
+     
+	<div id="login-panel">
+	<form action="" method="post">
+    <p>
+    <span class="sign">sign in</span> <br/>
+    <label class="user">user name
+    <input name="username" type="text" value="" />
+    </label> <br /><br />
+    <label class="password">password
+    <input name="password" type="password" value="" />
+    </label>
+    <input class="enter" type="submit" name="submit" value="Login" /><br/><br />
+    <span class="phrase">don't have an account. ask one by providing your email</span>
+    <label class="email">email
+    <input name="email" type="email" value="" />
+    <input class="enter" type="submit" name="submit" value="sign up" />
+    </label>
+    </p>
+    </form>
+    </div>
+   
+   </div> <!--fin de menu-->  
+ 
+ </div><!--fin de barre-top-->  
+    
+   <div class="effacement"></div>
+ 
+    
+	<div id="contener">
+ 
+	<!--contenu colone gauche -->    	
+    <div id="sidebar-left">
+    	<div id="barre-left">
+        
+        <div id="sid_1">
+        <h3>informations</h3>
+      		<p>
+			create: xx .xx .xxx <br/>
+			updated: xx .xx .xxx <br/>
+            analysis: 56 <br/>
+            documents: 200<br/>
+            messages: 555
+		 	</p>
+         </div>
+         
+         <div id="sid_2">
+         <h3>participants</h3>
+         
+         <div id="utilisateur">
+      	<span class="name">bill</span>
+		<div id="contain1" class="contain-left"> </div>      
+
+         <span class="name">bob</span>
+		<div id="contain2" class="contain-left"> </div>
+               
+         <span class="name">john</span>
+        <div id="contain3" class="contain-left"> </div>       
+        
+         <span class="name">admin</span>
+   		<div id="contain4" class="contain-left"></div> 
+                  
+ 		</div>
+        
+        </div>
+                  
+         <div id="sid_3">         
+         <h3>phases</h3>                  
+         </div>
+         
+    	</div>  <!--fin de barre-left-->
+    	</div><!--fin de sidebar-left-->
+    
+   
+    
+	<!--contenu central -->       
+    <div id="main-content">
+    
+    
+        <div class="rendu-suite">
+    	<h3>tubes (analysis.py/12:02 21.06.2011)</h3> <span class="data3">10.03.2012</span>
+        <div class="comment2b"></div>
+      <a href="images/rendu-script2.png" rel="lightbox"> <img src="images/rendu-script.png" alt="resultat-script"  /></a> 
+    </div>
+    
+    <div class="effacement"></div>
+	<div id="trait"></div>
+        
+        <div class="commentaires-suite" id="el_1_comments">
+        <div class="info-bul-img"></div>
+                	<div class="comments_list">
+                    	
+				
+	<div class="comment1">
+          <h2>jean-philippe cointet</h2> <span class="data2A">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+           </div>
+            <div class="comment1">
+            <h3>jean-philippe cointet</h3> <span class="data2">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            </div>
+            
+            <div class="comment1" id="test12">
+            <h3>jean-philippe cointet</h3> <span class="data2">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+            <div class="content">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            </div>
+            </div>
+            
+            </div><!-- /comments_list -->
+            
+         <div class="comment-text">
+            <form name="fff">
+  			<textarea id="dmc" onFocus="if(this.value!='')this.value=''"; ></textarea>
+			</form>
+            
+            <div class="img-commentaire">
+            <div class="bouton-com">
+            <a href="#"><img src="images/bul-com.png" alt="" /><span class="comm">comment</span></a>
+            </div></div>           
+         </div>
+        
+        </div><!--fin div commentaires -->   
+        	
+        
+    <div class="effacement"></div>
+	
+    
+    <div class="comment2">
+    	<h3>lise cornilleau</h3> <span class="data3">10.03.2012</span>
+        <div class="comment2b"></div>
+    <p>veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis es</p>
+       
+    </div>
+    
+    
+     <div class="commentaires" id="el_2_comments">
+      <div class="info-bul-img"></div>
+
+        <div class="comments_list2">
+
+        	<div class="comment1">
+            <h2>jean-philippe cointet</h2> <span class="data2A">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+           </div>
+            <div class="comment1">
+            <h3>jean-philippe cointet</h3> <span class="data2">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            </div>
+            <div class="comment1" id="test13">
+            <h3>jean-philippe cointet</h3> <span class="data2">23:12 12.05.2012</span>
+               <div class="effacement"></div>
+               <div class="content">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+            </div>
+            </div>
+            
+            </div><!-- /comments_list2 -->
+
+            <div class="comment-text">
+            <form name="fff">
+  			<textarea id="dmc2" onFocus="if(this.value!='')this.value=''"; ></textarea>
+			</form>
+            <div class="img-commentaire">
+            <div class="bouton-com">
+            <a href="#"><img src="images/bul-com.png" alt="" /><span class="comm">comment</span></a>
+            </div></div> 
+         	</div>
+        
+        </div>   
+    
+    	</div><!--fin contenu central -->   
+   
+    
+<!--contenu colonne droite -->    	        
+    <div id="sidebar-right">
+       <div id="barre-right">
+       
+    	<!--<div id="sid1">
+        <a href="#"><h2>start an analysis</h2></a>
+     	<a href="#"><h3>add a document</h3></a>
+        </div>-->
+        
+        
+        <div id="barre-right">
+          
+    	<div id="sid2-suite">
+      	<h4>queued scripts</h4>
+                      
+       	<div id="analysis1">
+       	<span class="an1">analysis.py</span>
+       	<span class="an2">10.03.2012</span> 
+        <div id="canvas1"></div>			
+  		<span class="an3">tam kien duong</span>                       
+    	</div>
+ 
+    	<div id="analysis2">
+    	<span class="an1">analysis.py</span>
+       	<span class="an2">10.03.2012</span> 
+        <div id="canvas2"></div>
+       	<span class="an3">tam kien duong</span>         
+    	</div>
+    
+       	<div id="analysis3">
+       	<span class="an1">analysis.py</span>
+       	<span class="an2">10.03.2012</span>
+        <div id="canvas3"></div>
+        <span class="an3">tam kien duong</span>    
+       	</div>
+        </div> <!-- fin side2 --> 
+             
+   	<div id="logo"></div>
+    
+           </div> <!--fin barre<-->       
+    </div><!--fin colonne droite -->
+        	            	       
+	</div><!--fin du contener--> 
+           
+</div><!--fin du conteneur principal-->
+     
+</body>
+</html>
---
+++ templates/js/fonctions_form_barre_comment2.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+	
+	//-------- formulaire: login/password----------
+						   
+    $("#login-link").click(function(){
+        $("#login-panel").slideToggle(200);
+    });
+	
+	
+	//-------- commentaire/saut de ligne----------
+
+	  $('textarea').simpleautogrow();
+	  
+
+	//--------fonction pour les barres de progression----------
+	
+	for(var i = 1; i < 7; i++){
+		var a = Math.floor(Math.random() * 100);
+		
+		script_progress_bar("container"+i, "canvas"+i, a );
+	}
+	
+
+	
+	for(var i = 1; i < 5; i++){
+		var a = Math.floor(Math.random() * 50);
+		var b = Math.floor(Math.random() * 50);
+		user_progress_bar("container"+i, "contain"+i, a, b);
+	}
+	
+	
+	//--------fonction pour l'ajout des commentaires 1----------
+	
+	$("#el_1_comments a").click(function(){
+		var div = $("#test12").clone();
+		
+		$(div).children('.content').html("<p>"+ $("#dmc").val() +"</p>");
+		
+		$(".comments_list").append(div);
+		$(div).hide();		
+		$(div).slideDown("slow");			
+
+		
+	});
+	
+	
+	//--------fonction pour l'ajout des commentaires 2----------
+	
+	$("#el_2_comments a").click(function(){
+		var div = $("#test13").clone();
+		
+		$(div).children('.content').html("<p>"+ $("#dmc2").val() +"</p>");
+		
+		$(".comments_list2").append(div);
+		$(div).hide();
+		$(div).slideDown("slow");
+
+		
+	});
+	
+	//--------fonction pour l'ajout des commentaires 3----------
+	
+	$("#el_3_comments a").click(function(){
+		var div = $("#test14").clone();
+		
+		$(div).children('.content').html("<p>"+ $("#dmc3").val() +"</p>");
+		
+		$(".comments_list3").append(div);
+		$(div).hide();
+		$(div).slideDown("slow");
+		
+	});
+	
+	
+	//--------fonction pour l'ajout des commentaires 4----------
+	
+	$("#el_4_comments a").click(function(){
+		var div = $("#test15").clone();
+		
+		$(div).children('.content').html("<p>"+ $("#dmc4").val() +"</p>");
+		
+		$(".comments_list4").append(div);
+		$(div).hide();
+		$(div).slideDown("slow");
+		
+	});
+	
+});
+
+	//--------fonction pour le formulaire: login/password----------
+
+	$(document).keydown(function(e) {
+    if (e.keyCode == 27) {
+        $("#login-panel").hide(0);
+  	  }
+});
