823dabe611f09beed2d7601d24138aef5edd84ac
chloé duloquin <chloeduloquin@hotmail.fr>
2012-05-09 16:07:46 +0200

    new

---
+++ templates/cortext_accueil.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Cortext Manager</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Plateforme Cortext Manager" />
+<meta name="keywords" content="manager,corpus,texte" />  
+<link href="css/reset.css" type="text/css" rel="stylesheet" />  
+<!--<link href="css/style-general.css" type="text/css" rel="stylesheet" />
+--><link href="css/style-menu-accueil.css" type="text/css" rel="stylesheet" />
+<link href="css/style-accueil.css" type="text/css" rel="stylesheet" />
+
+<link href="css/formulaire-accueil.css" type="text/css" rel="stylesheet" />
+
+
+</head>
+<body onload="">
+  	    
+<!--    <div id="barre-top">
+-->    
+    <div id="menu">
+    <a href="#"><img src="images/logo-petit.png" alt="#" /></a> 
+    <!--<a href="#"><img src="images/home2.png" alt="#" /></a>     
+    <ul>
+    <li class="menu1"><a href="#"> algues vertes</a></li>  
+   	<li class="menu2"><a id="login-link" href="#" title="Login"> tam kien duong</a></li>
+    </ul>    
+     
+	<div id="login-panel">
+	<form action="" method="post">
+    <p>
+    <span class="sign">sign in</span> <br/>
+    <label class="user">user name
+    <input name="username" type="text" value="" />
+    </label> <br /><br />
+    <label class="password">password
+    <input name="password" type="password" value="" />
+    </label>
+    <input class="enter" type="submit" name="submit" value="Login" /><br/><br />
+    <span class="phrase">don't have an account. ask one by providing your email</span>
+    <label class="email">email
+    <input name="email" type="email" value="" />
+    <input class="enter" type="submit" name="submit" value="sign up" />
+    </label>
+    </p>
+    </form>
+    </div>-->
+   
+   </div> <!--fin de menu-->  
+ 
+<!-- </div>--> <!--fin barre-top pour position:fixed-->
+    
+   <div class="effacement"></div>
+ 
+    
+	<div id="contener-accueil">
+
+	  <div id="accueil"></div>
+      
+      
+      <div id="login_accueil">
+      
+		<form class="member" action="" method="post">
+        <span class="sign">sign in</span> <br/>
+     <label class="user">username
+        <input name="username" type="text" value="" />
+        </label> <br /><br />
+        <label class="password">password
+        <input name="password" type="password" value="" />
+        </label>
+        <input class="enter" type="submit" name="submit" value="Login" />    
+        </form>
+    
+    </div>
+
+      <div id="login_accueil2">
+      
+      <form class="member2" action="" method="post">
+        <span class="phrase">apply for an account</span> <br/>
+     <label class="user2">username
+        <input name="username" type="text" value="" />
+        </label> <br /><br />
+        <label class="password2">password
+        <input name="password" type="password" value="" />
+        </label><br /><br />
+        
+        <label class="first">first name
+        <input name="first" type="password" value="" />
+        </label> <br /><br />
+        
+        <label class="last">last name
+        <input name="last" type="password" value="" />
+        </label> <br /><br />
+        
+        <label class="email">email 
+        <input name="email" type="password" value="" />
+        </label> <br /><br />
+        
+        <label class="institut">institution
+        <input name="institut" type="password" value="" />
+        </label> <br /><br />
+        
+        <input class="enter2" type="submit" name="submit" value="Login" />    
+        </form>
+      
+      
+      
+      
+      
+      </div> 	        
+    
+    </div><!--fin du contener--> 
+      
+</body>
+</html>
