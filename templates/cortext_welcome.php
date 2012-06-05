ff321bc0889b3130a08c9ffe683f164ae24ab856
chloé duloquin <chloeduloquin@hotmail.fr>
2012-06-05 11:27:43 +0200

    template

--- templates/cortext_welcome.php
+++ templates/cortext_welcome.php
@@ -8,37 +8,51 @@
 <link href="css/reset.css" type="text/css" rel="stylesheet" />  
 <link href="css/style-menu-accueil.css" type="text/css" rel="stylesheet" />
 <link href="css/style-welcome.css" type="text/css" rel="stylesheet" />
-
 </head>
+
 <body onload="">
     
     <div id="menu"> 
     <a href="#"><img src="images/home2.png" alt="#" /></a>      
-   </div> <!--fin de menu-->  
+   	</div> <!--fin de menu-->  
  
-    
 	<div class="effacement"></div>
+    
 	<div id="body-welcome">
-     </div>
+    </div>
+     
 	<div id="contenu-welcome">
     
 		<div id="text_net">
         <h2>Networks</h2>
-        <p>Klanten helpen<br/> elkaar op een<br/> Customer Support<br/> 			        Community.Service<br/> gaat verder.</p>        
+        <p>Klanten helpen<br/> 
+        elkaar op een<br/> 
+        Customer Support<br/>
+      	Community.Service<br/> 
+        gaat verder.</p>        
         </div>
         
 		<div id="text_net2">
         <h2>Sociology</h2>
-        <p>Klanten helpen<br/> elkaar op een<br/> Customer Support<br/> 			        Community.Service<br/> gaat verder.</p>  
+        <p>Klanten helpen<br/> 
+        elkaar op een<br/> 
+        Customer Support<br/> 			        
+        Community.Service<br/> 
+        gaat verder.</p>  
         </div>
         
 		<div id="text_net3">
         <h2>Project</h2>
-        <p>Klanten helpen<br/> elkaar op een<br/> Customer Support<br/> 			        Community.Service<br/> gaat verder.</p>  
+        <p>Klanten helpen<br/> 
+        elkaar op een<br/> 
+        Customer Support<br/> 			        
+        Community.Service<br/> 
+        gaat verder.</p>  
         </div>
     
-    </div><!--fin du contener-->
-        <div id="traits"></div>
+    	</div><!--fin du contener-->
+        
+    <div id="traits"></div>
  
     <div id="bas-welcome">
     
@@ -56,23 +70,27 @@
         
     	<div id="bas4">
         <div id="logo_cortext"></div>
-        <p>iste natus error sit voluptatem,
-accusantium doloremque.iste natus 
-error sit voluptatem,accusantium 
-doloremqueiste natus error sit volup
-tatem,accusantium doloremqueiste 
-natus error sit voluptatem,
-accusantium dolor emqueiste natus
-error sit voluptatem,accusantium 
-doloremqueiste natuserror sit,
-voluptatem accusantium
-<br/><br/>
-<span class="team">organisation:</span><span class="team2"> Cortext Team</span>
-</p>
+        <p>
+        iste natus error sit voluptatem,
+        accusantium doloremque.iste natus 
+        error sit voluptatem,accusantium 
+        doloremqueiste natus error sit volup
+        tatem,accusantium doloremqueiste 
+        natus error sit voluptatem,
+        accusantium dolor emqueiste natus
+        error sit voluptatem,accusantium 
+        doloremqueiste natuserror sit,
+        voluptatem accusantium
+		<br/><br/>
+		<span class="team">organisation:</span>
+        <span class="team2"> Cortext Team</span>
+		</p>
         </div>
+        
     	<div id="bas5">
         <h3>Cooperation</h3>
-        </div>    
+        </div>
+            
     </div>
       
 </body>
