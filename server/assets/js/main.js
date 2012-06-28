80b69dc4fcef237ce7c63ca1208ec2e7d91895d7
duong tam kien <tk@deveha.com>
2012-06-28 15:23:26 +0200

    refactoring

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -236,24 +236,3 @@ function to_participation_bar(e){
 
 	$(e).prepend(canvas);
 }
-
-String.prototype.lpad = function(padString, length) {
-	var str = this;
-    while (str.length < length)
-        str = padString + str;
-    return str;
-}
-
-format_date = function(date){
-	var date = new Date(date);
-
-	var d = date.getDate();
-		d = String(d).lpad("0", 2);
-
-	var m = date.getMonth()+1;
-		m = String(m).lpad("0", 2);
-
-	var y = date.getFullYear()
-
-	return d+"."+m+"."+y;
-}
