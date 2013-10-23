d3c7f34087f7e2e72842f1affd1c978fb869904e
Philippe Breucker <breucker@ifris.org>
2013-10-23 18:42:39 +0200

    test post elements

--- private/api/test/test.js
+++ private/api/test/test.js
@@ -3,25 +3,44 @@ var request = require('supertest')
   , express = require('express');
 
 var app = require('../app.js');
-/*
-request(app)
-  .get('/elements')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '20')
-  .expect(200)
-  .end(function(err, res){
-    if (err) throw err;
+
+
+describe('GET /', function(){
+  it("should return code 200 and welcome tag", function (){
+      request(app)
+        .get('/')
+        .expect('Content-Type', /text/)
+        .expect(200)
+        .end(function(err, res){
+          if (err) throw err;
+        });    
   });
-  */
-  describe('/elements', function(){
-    it("should return code 200 and expected element list", function (){
-        request(app)
-          .get('/elements')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .end(function(err, res){
-            if (err) throw err;
-          });    
-    })
-    
-  })
+});
+
+describe('GET /elements', function(){
+  it("should return code 200 and expected element list", function (){
+      elmt = request(app).get('/elements');
+      console.log (elmt);
+        elmt.expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res){
+          if (err) throw err;
+          res.send('coucou');
+          
+        });    
+  });
+});
+
+
+describe('POST /elements', function(){
+  it("should return code 200 and welcome tag", function (){
+      request(app)
+        .post('/elements')
+        .send({ author: 1, project: 1, type: 'Message', content: 'Hey dude !' })
+        .expect('Content-Type', /text/)
+        .expect(201)
+        .end(function(err, res){
+          if (err) throw err;
+        });    
+  });
+});
